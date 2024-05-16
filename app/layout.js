30488107466f8fa9043a4bd17d2a1c55f3034ba8
tunwit <tunwit2458@gmail.com>
2024-05-17 00:35:36 +0700

    remove provider directory

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Provider from "./components/Provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,9 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Provider>
         <body className={inter.className}>{children}</body>
-      </Provider>
     </html>
   );
 }
